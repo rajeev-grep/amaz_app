6ad5971c0ca4792020056fb1336d729dd3391d84
rajeev <rajeev@grepruby.com>
2013-02-06 15:15:33 +0530

    amaz_demo_app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Amaz::Application.config.secret_token = '05c58c3b57d531abe75f605b3d9e94d92c1cb1611944a96a82673d72bcf366b1af86a3b72d965547140a64e583b15d6dbbb502c85999edc4da32045feaca85e7'
